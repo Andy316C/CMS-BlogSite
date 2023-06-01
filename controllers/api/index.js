1c75ae160dd8acfb650aeadf53204b0230827a58
andrew clark <andy-clark316@hotmail.com>
2023-06-01 16:40:00 +1000

    updated blog data in controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('..');
-const withAuth = require('../../middleware/auth');
-
-// const userRoutes = require('./auth_routes');
-// const browseRoutes = require('./browse-route');
-// const suggesterRoutes = require('./suggester_routes');
+// const apiRoutes = require('..');
+// const withAuth = require('../../middleware/auth');
 const authRoutes = require('./auth_routes');
-// router.use('/users', userRoutes);
-// router.use('/browse', browseRoutes);
-// router.use('/suggester', suggesterRoutes);
+
 router.use('/auth', authRoutes);
 
 module.exports = router;
--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Blog, User } = require('../model');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
   try {
@@ -30,8 +30,30 @@ router.get('/', async (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login', {loggedIn: req.session?.loggedIn, user: req.session?.user}); // Render the login.handlebars template
 });
+router.get('/logout', (req, res) => {
+  res.redirect('/api/auth/logout');
+});
 router.get('/sign-up', (req, res) => {
   res.render('signup', {loggedIn: req.session?.loggedIn, user: req.session?.user}); // Render the signup.handlebars template
+});
+
+router.get('/myBlogs', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('myBlogs', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
